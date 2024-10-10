abf33363e5096287e17b9d03167c5f7dbaf71621
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-10-10 22:18:46 +0530

    changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+    return <div className="app__container">
+  
+      <div className="question__container">
+        <span className="question">Q Whats up doc</span>
+      </div>
+  
+      <div className="answers__container">
+        <span className="answer">1. Whats up doc</span>
+        <span className="answer">2. Whats up doc</span>
+        <span className="answer">3. Whats up doc</span>
+        <span className="answer">4. Whats up doc</span>
+      </div>
+  
+    </div>;
+  }
+  
+  export default App;
+  
