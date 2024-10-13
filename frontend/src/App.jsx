99199992f976e74ba979f981bc7dff99d50e543b
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-10-13 18:39:05 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,40 @@
+import { useState, useEffect } from "react";
+import QRCode from "react-qr-code";
+
+let data = [
+    {
+        "question": "who's the best dude?",
+        "answer": "no",
+        "answers": [
+            'yes, no, maybe, pass'
+        ],
+        "id": 1232
+    },
+    {
+        "question": "who's the best dude?",
+        "answer": "no",
+        "answers": [
+            'yes, no, maybe, pass'
+        ],
+        "id": 12332
+    },
+    {
+        "question": "who's the best dude?",
+        "answer": "no",
+        "answers": [
+            'yes, no, maybe, pass'
+        ],
+        "id": 12352
+    }
+]
+
 function App() {
+    const [qr, setQr] = useState("");
+
+
+    useEffect(() => {
+        console.log('ue run')
+    }, [])
   return (
     <div className="app__container">
         <nav className="nav">KBC Quiz</nav>
@@ -16,7 +52,7 @@ function App() {
         </div>
 
         <div className="qr__container container">
-            QR CODE
+            <QRCode size={150} value={qr}></QRCode>
         </div>
     </div>
   );
